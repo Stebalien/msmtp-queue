17ffccaa70baed0178abf01738f3ea798b8fd457
Steven Allen <steven@stebalien.com>
2026-01-22 08:46:36 -0800

    nix: format

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       eachSystem = nixpkgs.lib.genAttrs [
         "i686-linux"
@@ -15,44 +16,55 @@
       ];
     in
     {
-      packages = eachSystem (system:
+      packages = eachSystem (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
-          msmtpq = (pkgs.resholve.mkDerivation {
-            pname = "msmtpq";
-            version = "1.0.0";
-            src = ./.;
-            strictDeps = true;
-            nativeBuildInputs = with pkgs; [ gnumake m4 ];
+          msmtpq =
+            (pkgs.resholve.mkDerivation {
+              pname = "msmtpq";
+              version = "1.0.0";
+              src = ./.;
+              strictDeps = true;
+              nativeBuildInputs = with pkgs; [
+                gnumake
+                m4
+              ];
 
-            installPhase = ''
-              mkdir -p $out/bin/
-              install -Dm755 msmtpq msmtpq-flush msmtpq-queue $out/bin
-              install -Dm644 -t "$out/share/systemd/user/" systemd/*
-            '';
+              installPhase = ''
+                mkdir -p $out/bin/
+                install -Dm755 msmtpq msmtpq-flush msmtpq-queue $out/bin
+                install -Dm644 -t "$out/share/systemd/user/" systemd/*
+              '';
 
-            solutions = {
-              msmtpq = {
-                interpreter = "${pkgs.bash}/bin/bash";
-                scripts = [ "bin/msmtpq" "bin/msmtpq-flush" "bin/msmtpq-queue" ];
-                inputs = with pkgs; [
-                  util-linux
-                  coreutils
-                  findutils
-                  gnused
-                  (msmtp.override { withScripts = false; })
-                ];
-                execer = [
-                  "cannot:${pkgs.util-linux}/bin/flock"
-                ];
+              solutions = {
+                msmtpq = {
+                  interpreter = "${pkgs.bash}/bin/bash";
+                  scripts = [
+                    "bin/msmtpq"
+                    "bin/msmtpq-flush"
+                    "bin/msmtpq-queue"
+                  ];
+                  inputs = with pkgs; [
+                    util-linux
+                    coreutils
+                    findutils
+                    gnused
+                    (msmtp.override { withScripts = false; })
+                  ];
+                  execer = [
+                    "cannot:${pkgs.util-linux}/bin/flock"
+                  ];
+                };
               };
-            };
-          }).overrideAttrs (old: {
-            preFixup = ''
-              substituteInPlace "$out/share/systemd/user/msmtp-queue.service" \
-                --replace-fail '/usr/local/bin/msmtpq-flush' "$out/bin/msmtpq-flush"
-            '' + old.preFixup;
-          });
+            }).overrideAttrs
+              (old: {
+                preFixup = ''
+                  substituteInPlace "$out/share/systemd/user/msmtp-queue.service" \
+                    --replace-fail '/usr/local/bin/msmtpq-flush' "$out/bin/msmtpq-flush"
+                ''
+                + old.preFixup;
+              });
         in
         {
           inherit msmtpq;
@@ -60,11 +72,19 @@
         }
       );
 
+      formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+
       overlays.default = final: prev: {
         inherit (self.packages.${final.system}) msmtpq;
       };
 
-      nixosModules.default = { lib, config, pkgs, ... }:
+      nixosModules.default =
+        {
+          lib,
+          config,
+          pkgs,
+          ...
+        }:
         let
           cfg = config.services.msmtpq;
         in
